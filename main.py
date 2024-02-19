3c29f55ab6b8d5c6a3c48a1c30a5996fd5b8c41e
Coderguy71 <ishaantrivedi0608@gmail.com>
2024-02-19 18:51:13 -0500

    Added the info and the first part of the page

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
 with col1:
-    st.image("images/image.jpg", width=600)
+    st.image("images/image.jpg", width=800)
 
 with col2:
     st.title("Ishaan Trivedi")
     content = """
-    Hi, I am Ishaan Trivedi! I am a Python and Java programmer, and a student at Morris Hill's Academy of Math, Science, and Engineering.
-    I am learning how to code in Python, and this is my portfolio website for all of my Python creations!"""
+    Hi, I am Ishaan Trivedi! I am a Python and Java programmer, and a student at Morris Hill's Academy of Math, Science, 
+    and Engineering.I am learning how to code in Python, and this is my portfolio website for all of my Python creations!
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+     architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+     aut odit aut fugit"""
     st.info(content)
+
+st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+
